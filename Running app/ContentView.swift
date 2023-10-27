acd6716a92531b4a65a29b1e30e7b20e20d3772b
rivboyj <rijo7618@colorado.edu>
2023-10-27 11:29:22 -0600

    Initial Commit

---
+++ Running app/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Running app
+//
+//  Created by River on 10/27/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Running app/Running_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  Running_appApp.swift
+//  Running app
+//
+//  Created by River on 10/27/23.
+//
+
+import SwiftUI
+
+@main
+struct Running_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
